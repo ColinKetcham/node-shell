7ca923c170dac41376299c4c8485f830301c974c
Tylen Carroll <tylenwcarroll@gmail.com>
2022-04-22 15:03:50 -0400

    finished unitl extra credit

--- bash.js
+++ bash.js
@@ -3,10 +3,11 @@ process.stdout.write("prompt > ");
 const pwd = require("./pwd");
 const ls = require("./ls");
 const cat = require("./cat");
+const curl = require("./curl");
 
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim().split(" ");
-  console.log(cmd);
+  // console.log(cmd);
 
   if (cmd[0] === "pwd") {
     pwd();
@@ -15,6 +16,8 @@ process.stdin.on("data", (data) => {
   } else if (cmd[0] === "cat") {
     //process.stdout.write(cmd[1]);
     cat(cmd[1]);
+  } else if (cmd[0] === "curl") {
+    curl(cmd[1]);
   } else {
     process.stdout.write("You typed: " + cmd);
     process.stdout.write("\nprompt > ");
--- cat.js
+++ cat.js
@@ -1,5 +1,15 @@
-// const fs = require("fs");
+const fs = require("fs");
 
 module.exports = (fileName) => {
-  process.stdout.write("got to cat:", fileName);
+  // process.stdout.write("got to cat:");
+
+  fs.readFile(`./${fileName}`, "utf8", function (err, data) {
+    if (err) {
+      throw err;
+    } else {
+      process.stdout.write(data);
+    }
+  });
+
+  process.stdout.write("\nprompt > ");
 };
