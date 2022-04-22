380c5dea83e456601ca93c417bd11b141229c427
Colin Ketcham <Colin.Ketcham@gmail.com>
2022-04-22 10:06:33 -0700

    cat currently broken

--- bash.js
+++ bash.js
@@ -2,14 +2,19 @@ process.stdout.write("prompt > ");
 
 const pwd = require("./pwd");
 const ls = require("./ls");
+const cat = require("./cat");
 
 process.stdin.on("data", (data) => {
-  const cmd = data.toString().trim();
+  const cmd = data.toString().trim().split(" ");
+  console.log(cmd);
 
-  if (cmd === "pwd") {
+  if (cmd[0] === "pwd") {
     pwd();
-  } else if (cmd === "ls") {
+  } else if (cmd[0] === "ls") {
     ls();
+  } else if (cmd[0] === "cat") {
+    //process.stdout.write(cmd[1]);
+    cat(cmd[1]);
   } else {
     process.stdout.write("You typed: " + cmd);
     process.stdout.write("\nprompt > ");
---
+++ cat.js
@@ -0,0 +1,5 @@
+// const fs = require("fs");
+
+module.exports = (fileName) => {
+  process.stdout.write("got to cat:", fileName);
+};
